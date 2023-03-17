370983f1d6fcce5dc383ba35c2bd0603f2697fc8
Milan Bohacek <244656@muni.cz>
2023-03-17 09:35:58 +0100

    create router configs, update compose files

---
+++ router_scripts/wan_router.sh
@@ -0,0 +1,13 @@
+#!/bin/vbash
+source /opt/vyatta/etc/functions/script-template
+configure
+set protocols static route 0.0.0.0/0 next-hop 192.168.50.250
+set protocols static route 192.168.91.0/24 next-hop 192.168.50.11
+set protocols static route 192.168.92.0/24 next-hop 192.168.50.12
+set nat source rule 100 outbound-interface 'eth0'
+set nat source rule 100 source address '192.168.91.0/24'
+set nat source rule 100 source address '192.168.92.0/24'
+set nat source rule 100 source address '192.168.93.0/24'
+set nat source rule 100 translation address masquerade
+commit
+exit
