5089800732d202956deaf16ff51668753e30f18c
Milan Boháček <244656@mail.muni.cz>
2023-10-23 15:07:26 +0200

    update agent create CLI command

--- dr_emu/cli/agent.py
+++ dr_emu/cli/agent.py
@@ -6,64 +6,24 @@ from dr_emu.cli import UTyper
 import typer
 from typing_extensions import Annotated
 
-from dr_emu.lib.util import InstallChoice, AgentRole
+from dr_emu.lib.util import AgentRole
 from dr_emu.models import AgentPypi, AgentGit, AgentLocal
 from dr_emu.lib import exceptions
 from dr_emu.controllers import agent as agent_controller
 from dr_emu.lib.logger import logger
 
 agent_typer = UTyper()
+source_typer = UTyper()
+agent_typer.add_typer(source_typer, name="create", help="Select source for Agent installation")
 
 
-@agent_typer.command("create")
-async def create(
-    name: Annotated[
-        str,
-        typer.Argument(help="agent name"),
-    ],
-    role: Annotated[
-        AgentRole,
-        typer.Argument(help="Agent role"),
-    ] = AgentRole.attacker.value,
-    source: Annotated[
-        InstallChoice,
-        typer.Argument(
-            help="type of source from which should be agent installed.(This will trigger a prompt)",
-        ),
-    ] = InstallChoice.git.value,
-):
+async def create_agent(name, role, source):
     """
     Create Agent.
     """
 
-    if source == InstallChoice.git:
-        username = typer.prompt("Username", type=str, default="oauth2")
-        token = typer.prompt("Personal/Repository access token (hidden input)", type=str, hide_input=True)
-        package_name = typer.prompt("Python package name", type=str)
-        git_project_url = typer.prompt("Clone with HTTPS repository url", type=str)
-
-        if (parsed_url := parse(git_project_url, check_domain=False)).valid is False:
-            print("[bold red]Invalid repository url![/bold red]")
-            return
-
-        installation_type = AgentGit(
-            access_token=token,
-            username=username,
-            package_name=package_name,
-            host=parsed_url.host,
-            owner=parsed_url.owner,
-            repo_name=parsed_url.repo,
-        )
-    elif source == InstallChoice.pypi:
-        package_name = typer.prompt("Package name")
-        installation_type = AgentPypi(package_name=package_name)
-    else:
-        package_name = typer.prompt("Package name")
-        path_to_agent = typer.prompt("Path to agent")
-        installation_type = AgentLocal(path=path_to_agent, package_name=package_name)
-
     try:
-        agent = await agent_controller.create_agent(name, role.value, installation_type)
+        agent = await agent_controller.create_agent(name, role.value, source)
         print(
             f"[bold green]Agent with name: {agent.name} and id: {agent.id} has been created and installed[/bold green]"
         )
@@ -124,3 +84,102 @@ async def delete_agent(
         print(f"Agent with name: {agent.name} and id: {agent.id} has been deleted")
     except NoResultFound:
         print(f"[bold red]Agent with ID: {agent_id} doesn't exist![/bold red]")
+
+
+@source_typer.command("git")
+async def git_source(
+    agent_name: Annotated[
+        str,
+        typer.Option("--name", "-n", help="agent name", prompt="Agent name"),
+    ],
+    role: Annotated[
+        AgentRole,
+        typer.Option("--role", "-r", help="Agent role", prompt="Agent role"),
+    ],
+    package_name: Annotated[
+        str,
+        typer.Option("--package", "-p", help="Python package name", prompt="Python package name"),
+    ],
+    url: Annotated[
+        str,
+        typer.Option(
+            "--url",
+            "-l",
+            help="Clone with HTTPS repository url",
+            prompt="Clone with HTTPS repository url",
+        ),
+    ],
+    token: Annotated[
+        str,
+        typer.Option(
+            "--token", "-t", help="Git access token", prompt="Personal/Repository access token (hidden input)"
+        ),
+    ],
+    username: Annotated[
+        str,
+        typer.Option(
+            "--username",
+            "-u",
+            help="Git username",
+        ),
+    ] = "oauth2",
+):
+    if (parsed_url := parse(url, check_domain=False)).valid is False:
+        print("[bold red]Invalid repository url![/bold red]")
+        return
+
+    installation_type = AgentGit(
+        access_token=token,
+        username=username,
+        package_name=package_name,
+        host=parsed_url.host,
+        owner=parsed_url.owner,
+        repo_name=parsed_url.repo,
+    )
+
+    await create_agent(agent_name, role, installation_type)
+
+
+@source_typer.command("pypi")
+async def git_source(
+    agent_name: Annotated[
+        str,
+        typer.Option("--name", "-n", help="agent name", prompt="Agent name"),
+    ],
+    role: Annotated[
+        AgentRole,
+        typer.Option("--role", "-r", help="Agent role", prompt="Agent role"),
+    ],
+    package_name: Annotated[
+        str,
+        typer.Option("--package", "-p", help="Python package name", prompt="Python package name"),
+    ],
+):
+    installation_type = AgentPypi(package_name=package_name)
+    await create_agent(agent_name, role, installation_type)
+
+
+@source_typer.command("local")
+async def git_source(
+    agent_name: Annotated[
+        str,
+        typer.Option("--name", "-n", help="agent name", prompt="Agent name"),
+    ],
+    role: Annotated[
+        AgentRole,
+        typer.Option("--role", "-r", help="Agent role", prompt="Agent role"),
+    ],
+    package_name: Annotated[
+        str,
+        typer.Option("--package", "-p", help="Python package name", prompt="Python package name"),
+    ],
+    path: Annotated[
+        str,
+        typer.Option(
+            help="Path to the Agent project in CYST container",
+            prompt="Path to the Agent project in CYST container",
+        ),
+    ],
+):
+    installation_type = AgentLocal(path=path, package_name=package_name)
+    await create_agent(agent_name, role, installation_type)
